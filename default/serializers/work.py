a2712032a4ba9a5607753129619639595b41aa62
NikaShabashvili03 <shabashvilinika@yahoo.com>
2025-01-18 04:12:50 +0400

    default

--- default/serializers/work.py
+++ default/serializers/work.py
@@ -4,8 +4,6 @@ from .stage import StageSerializer
 
 
 class WorkSerializer(serializers.ModelSerializer):
-    stages = StageSerializer(many=True, read_only=True)
-
     class Meta:
         model = Work 
-        fields = ['id', 'name', 'stages']
+        fields = ['id', 'name']
